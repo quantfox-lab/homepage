aa42050109c492ab0545f95f4ff1c763b6e9fb31
Zach <zach@tama.me>
2022-10-15 14:53:05 +1030

    polish

--- themes/jetfox/source/js/firebase.js
+++ themes/jetfox/source/js/firebase.js
@@ -1,43 +1,23 @@
-<script type="module">
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.12.1/firebase-app.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.12.1/firebase-analytics.js";
 
-  // Import the functions you need from the SDKs you need
+// Your web app's Firebase configuration
 
-  import { initializeApp } from "https://www.gstatic.com/firebasejs/9.12.1/firebase-app.js";
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 
-  import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.12.1/firebase-analytics.js";
-
-  // TODO: Add SDKs for Firebase products that you want to use
-
-  // https://firebase.google.com/docs/web/setup#available-libraries
-
-
-  // Your web app's Firebase configuration
-
-  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-
-  const firebaseConfig = {
-
+const firebaseConfig = {
     apiKey: "AIzaSyCwGsmF-Hqq64fu_VToFkFGGTl3mGl9jcQ",
-
     authDomain: "jetfox-homepage.firebaseapp.com",
-
     projectId: "jetfox-homepage",
-
     storageBucket: "jetfox-homepage.appspot.com",
-
     messagingSenderId: "1007888062090",
-
     appId: "1:1007888062090:web:4d357a1eec840b180d7f73",
-
     measurementId: "G-P1WGSC1ETT"
+};
 
-  };
 
+// Initialize Firebase
 
-  // Initialize Firebase
+const app = initializeApp(firebaseConfig);
 
-  const app = initializeApp(firebaseConfig);
-
-  const analytics = getAnalytics(app);
-
-</script>
+const analytics = getAnalytics(app);
